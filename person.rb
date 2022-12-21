dddc5f30af00337bc86000ba408b6b482fde62fb
Mauricio Mendoza <rbreva@gmail.com>
2022-12-21 18:06:06 -0500

    Changes required

--- person.rb
+++ person.rb
@@ -1,10 +1,12 @@
 class Person
-  attr_accessor :name, :age, :id
+  attr_accessor :name, :age
+  attr_reader :id
 
   def initialize(age, name: 'Unkown', parent_permission: true)
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @id = rand(1...10)
   end
 
   private
@@ -17,6 +19,6 @@ class Person
   public
 
   def can_use_services?
-    return true if is_of_age? || @parent_permission
+    return true if of_age? || @parent_permission
   end
 end
