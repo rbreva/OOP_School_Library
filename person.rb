6c211a1126d92ab8af5af5d6cc1facbcc5ca8ffe
Mauricio Mendoza <rbreva@gmail.com>
2022-12-23 21:30:22 -0500

    changes required

--- person.rb
+++ person.rb
@@ -1,11 +1,10 @@
 require_relative 'nameable'
 
-class Person
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name: 'Unkown', parent_permission: true)
-    super
+  def initialize(age, name = 'Unknown', parent_permission = true)
     @age = age
     @name = name
     @parent_permission = parent_permission
